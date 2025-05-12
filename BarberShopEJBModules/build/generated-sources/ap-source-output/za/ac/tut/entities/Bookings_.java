123db275895b73c9d63910b4601550b171eb06ae
SekgobelaME <sekgobelatumi010426@gmail.com>
2025-05-12 21:54:13 +0200

    First commit

---
+++ BarberShopEJBModules/build/generated-sources/ap-source-output/za/ac/tut/entities/Bookings_.java
@@ -0,0 +1,26 @@
+package za.ac.tut.entities;
+
+import java.sql.Time;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import za.ac.tut.entities.Person;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2025-05-12T19:46:32")
+@StaticMetamodel(Bookings.class)
+public class Bookings_ { 
+
+    public static volatile SingularAttribute<Bookings, Date> date;
+    public static volatile SingularAttribute<Bookings, Person> person;
+    public static volatile SingularAttribute<Bookings, String> name;
+    public static volatile SingularAttribute<Bookings, Integer> numPeople;
+    public static volatile SingularAttribute<Bookings, String> style;
+    public static volatile SingularAttribute<Bookings, Long> id;
+    public static volatile SingularAttribute<Bookings, Time> time;
+    public static volatile SingularAttribute<Bookings, Date> creationDate;
+    public static volatile SingularAttribute<Bookings, String> branch;
+    public static volatile SingularAttribute<Bookings, String> contacts;
+    public static volatile SingularAttribute<Bookings, String> status;
+
+}
